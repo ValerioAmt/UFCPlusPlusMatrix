b06e0bfbda300f1dafc2bd2ea3639ea94bebe0ca
valerio <valerioamato@hotmail.it>
2023-03-10 16:58:58 +0100

    5 da provare

--- es5.cpp
+++ es5.cpp
@@ -7,30 +7,42 @@ int main()
 {
     const int MATRICE = 8;
     int matrice[MATRICE][MATRICE];
-    
-    for(int i = 0; i < MATRICE; i++) // da riga 13 a 22                                
+
+    for (int i = 0; i < MATRICE; i++) // da riga 13 a 22
     {
         for (int y = 0; y < MATRICE; i++)
         {
-                                                  // riempo la matrice con numeri casuali < 10    
-            matrice[i][y] = rand() % 10;                                                            // se i numeri sono divisibili per 3 li metto dentro l'array risultato
+            // riempo la matrice con numeri casuali < 10
+            matrice[i][y] = rand() % 10; // se i numeri sono divisibili per 3 li metto dentro l'array risultato
         }
-           
-    }    //stampo matrice da dx vs sx
-        for(int i = MATRICE; i > 0; i --){
-            for(int y = 0; y < MATRICE; y++){
+
+    } // stampo matrice da dx vs sx
+    for (int i = MATRICE; i > 0; i--)
+    {
+        for (int y = 0; y < MATRICE; y++)
+        {
             cout << matrice[i][y];
-            }
-         cout << endl;   
         }
-    
+        cout << endl;
+    }
+    // stampo colonne invertite
 
+    for (int i = 0; i < MATRICE; i++)
+    {
+        for (int y = MATRICE; y > 0; y--)
+        {
+            cout << matrice[i][y];
+        }
+        cout << endl;
+    }
+    // matrice trasposta
 
-
-
-
-
-
-
-
+    for (int i = MATRICE; i > 0; i--)
+    {
+        for (int y = MATRICE; i > 0; i--)
+        {
+            cout << matrice[i][y];
+        }
+        cout << endl;
+    }
 }
