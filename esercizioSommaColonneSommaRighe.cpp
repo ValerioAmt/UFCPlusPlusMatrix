89d535a069650fe5c4e4018f5d193ad68f4420f4
valerio <valerioamato@hotmail.it>
2023-03-14 17:52:06 +0100

    prova esercizio

---
+++ esercizioSommaColonneSommaRighe.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int righe = 10;
+    int colonne = 10;
+    int matriceRandom[righe][colonne];
+
+    for (int i = 0; i < righe; i++)
+    {
+        for (int j = 0; j < colonne; j++)
+        {
+            matriceRandom[i][j] = rand() % 10;  //riempo matrice con numeri random
+        }
+    }
+
+    for (int i = 0; i < righe; i++)
+    {
+        int sommaRighe = 0;
+
+        cout << endl;
+
+        for (int j = 0; j < colonne; j++)
+        {
+            sommaRighe += matriceRandom[i][j];
+            cout << matriceRandom[i][j] << "  ";
+        }
+        cout << " |" << sommaRighe << " ";
+    }
+    cout << endl;
+
+    for (int i = 0; i < colonne; i++)
+    {
+        cout << "---";
+    }
+    cout << endl;
+    for (int i = 0; i < colonne; i++)
+    {
+        int sommaColonne = 0;
+        for (int j=0; j<righe;  j++)
+        {
+        sommaColonne += matriceRandom[j][i];
+        }
+        cout << sommaColonne << " ";
+    }
+    
+}
